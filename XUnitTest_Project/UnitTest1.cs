9b2a03d815d1145258aa65d35ab221454d4a96ff
adarshnishad <adarsh.nishad@nagarro.com>
2023-04-05 14:31:56 +0530

    unit testing

---
+++ XUnitTest_Project/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+
+namespace XUnitTest_Project
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Console.WriteLine("Demo Test Case");
+
+        }
+    }
+}
